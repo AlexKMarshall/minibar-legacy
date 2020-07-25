07b74d3d45d58bc4bf536997e2cce7fb19a85470
Alex Marshall <alex.k.marshall83@gmail.com>
2020-07-25 18:35:14 +0100

    Install eslint on server

---
+++ server/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2020: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "eslint-config-prettier"],
+  parserOptions: {
+    ecmaVersion: 11,
+  },
+  rules: {},
+};
