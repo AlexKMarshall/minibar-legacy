cebc2ba41a56fb0327055afc8fb42efc23a7712b
Alex Marshall <alex.k.marshall83@gmail.com>
2020-07-26 13:03:21 +0100

    Add eslint config for prettier

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
-  extends: ["react-app", "plugin:testing-library/react"],
+  extends: [
+    "react-app",
+    "plugin:testing-library/react",
+    "eslint-config-prettier",
+  ],
   plugins: ["testing-library"],
 };
