0ce629281306158e21e8c3e1521d195196a9ca17
Alex Marshall <alex.k.marshall83@gmail.com>
2020-07-29 19:01:45 +0100

    Add avatar and search icon

--- client/src/containers/Discover.js
+++ client/src/containers/Discover.js
@@ -12,7 +12,28 @@ export default function Discover() {
 
   if (!drinks.length) return "Loading...";
   return (
-    <main className="min-h-screen p-6 bg-gray-300">
+    <main className="min-h-screen p-6 pt-10 bg-gray-300">
+      <div className="flex justify-between mb-6">
+        <img
+          src="https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.5&w=144&q=80"
+          alt=""
+          className="w-12 h-12 rounded-full"
+        />
+        <div className="flex items-center justify-center w-12 h-12 text-gray-800 bg-gray-200 rounded-full">
+          <svg
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="3"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            className="w-4 h-4"
+          >
+            <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+          </svg>
+        </div>
+      </div>
+      <h2 className="mb-4 text-2xl font-bold">Featured drinks</h2>
       <ul className="flex w-full overflow-x-auto">
         {drinks.map((drink) => (
           <li key={drink._id}>
