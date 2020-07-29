7ac9ac0c49e1fb41066fce14bb660cd51368f5b4
Alex Marshall <alex.k.marshall83@gmail.com>
2020-07-29 13:38:03 +0100

    Enable cors on server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = require("./routes");
+const cors = require("cors");
 
 const app = express();
 
 const PORT = 3001;
-
+app.use(cors());
 app.use("/api", router);
 
 app.listen(PORT, () =>
