6472a6430401f46c78afe28827cadd51a2434e9c
Alex Marshall <alex.k.marshall83@gmail.com>
2020-07-30 07:35:08 +0100

    Create script to retrieve all drinks from remote DB

--- server/utils/scraping.js
+++ server/utils/scraping.js
@@ -1,3 +1,44 @@
+const fetch = require("node-fetch");
+
+const aToZ = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+function fetchData() {
+  const fetches = aToZ.map((letter) =>
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${letter}`)
+      .then((res) => res.json())
+      .then(({ drinks }) => (drinks ? drinks.map(drinkItemTransform) : null))
+  );
+
+  return Promise.all(fetches).then(console.log);
+}
+
 function drinkItemTransform(cocktailDbDrink) {
   const {
     idDrink: id,
@@ -29,5 +70,7 @@ function transformIngredients(cocktailDbDrink) {
   }
   return ingredients;
 }
+
+fetchData();
 
 module.exports = { drinkItemTransform };
