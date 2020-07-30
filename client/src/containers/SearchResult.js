33031adc9cb001ee6512e5a73add18a0c0ee16fb
Alex Marshall <alex.k.marshall83@gmail.com>
2020-07-30 17:56:12 +0100

    Indicate favorites on search results

--- client/src/containers/SearchResult.js
+++ client/src/containers/SearchResult.js
@@ -58,7 +58,7 @@ export default function Discover() {
                 />
               </div>
             </div>
-            <div className="px-3 py-2">
+            <div className="flex-grow px-3 py-2">
               <Link to={`/recipe/${drink._id}`}>
                 <h3 className="font-semibold">{drink.name}</h3>
               </Link>
@@ -66,6 +66,21 @@ export default function Discover() {
                 {drink.ingredients.map(({ name }) => name).join(", ")}
               </p>
             </div>
+            {drink.isFav ? (
+              <div className="self-center ml-2 text-gray-600">
+                <svg
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  className="w-6 h-6 "
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </div>
+            ) : null}
           </li>
         ))}
       </ul>
