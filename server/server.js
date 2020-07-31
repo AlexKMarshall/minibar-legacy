e720bce45f5c5110982d48f0cd4bd59646e2ada7
Alex Marshall <alex.k.marshall83@gmail.com>
2020-07-31 18:12:31 +0100

    Create project level start scripts

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const authMiddleWare = require("./middleware/auth-handler");
 
 const app = express();
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use(authMiddleWare.addUser);
 app.use("/api", router);
