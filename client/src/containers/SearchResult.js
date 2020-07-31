a61b8d14172f8a061ac048b38f9b6f7ff56a5d0f
Alex Marshall <alex.k.marshall83@gmail.com>
2020-07-31 10:37:04 +0100

    Refactor Search to use CompactList component

--- client/src/containers/SearchResult.js
+++ client/src/containers/SearchResult.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 
 import { searchDrinks } from "../utils/api-client";
+import CompactList from "../components/CompactList";
 
 function useSearchDrinksByIngredient() {
   const [drinks, setDrinks] = useState([]);
@@ -31,44 +32,7 @@ export default function Discover() {
       <h2 className="mb-4 text-xl font-display">
         Search results for {searchTerm}
       </h2>
-      <ul className="space-y-3">
-        {drinks.map((drink) => (
-          <li key={drink._id} className="flex">
-            <div className="w-1/6">
-              <div className="relative overflow-hidden rounded pb-5/4">
-                <img
-                  src={drink.image}
-                  alt=""
-                  className="absolute object-cover w-full h-full"
-                />
-              </div>
-            </div>
-            <div className="flex-grow px-3 py-2">
-              <Link to={`/recipe/${drink._id}`}>
-                <h3 className="font-semibold font-display">{drink.name}</h3>
-              </Link>
-              <p className="text-xs">
-                {drink.ingredients.map(({ name }) => name).join(", ")}
-              </p>
-            </div>
-            {drink.isFav ? (
-              <div className="self-center ml-2 text-gray-600">
-                <svg
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  className="w-6 h-6 "
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-              </div>
-            ) : null}
-          </li>
-        ))}
-      </ul>
+      <CompactList drinks={drinks} />
     </>
   );
 }
