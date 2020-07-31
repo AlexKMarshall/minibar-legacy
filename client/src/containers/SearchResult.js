0e1ab146089969e9d759903c2f182f0076712c2d
Alex Marshall <alex.k.marshall83@gmail.com>
2020-07-31 10:38:25 +0100

    Remove unused imports

--- client/src/containers/SearchResult.js
+++ client/src/containers/SearchResult.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 import { searchDrinks } from "../utils/api-client";
 import CompactList from "../components/CompactList";
