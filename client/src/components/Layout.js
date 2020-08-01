a31cbbee60e778ba88c2db5120fa34217f3c9c61
Alex Marshall <alex.k.marshall83@gmail.com>
2020-08-01 12:58:44 +0100

    Extract LayoutHeader component

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -19,19 +19,25 @@ export default function Layout({ children }) {
   return (
     <>
       <SideNav isOpen={isNavOpen} close={() => setIsNavOpen(false)} />
-      <main className="min-h-screen p-6 pt-10 bg-gray-300">
-        <div className="flex justify-between mb-6">
-          <button onClick={toggleNav}>
-            <img
-              src="https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.5&w=144&q=80"
-              alt=""
-              className="w-12 h-12 rounded-full"
-            />
-          </button>
-          <SearchControl onSearchSubmit={onSearchSubmit} />
-        </div>
-        {children}
-      </main>
+      <LayoutHeader toggleNav={toggleNav} onSearchSubmit={onSearchSubmit} />
+      <main className="min-h-screen px-6 bg-gray-300">{children}</main>
     </>
+  );
+}
+
+function LayoutHeader({ toggleNav, onSearchSubmit }) {
+  return (
+    <header className="p-6 pt-10">
+      <div className="flex justify-between mb-6">
+        <button onClick={toggleNav}>
+          <img
+            src="https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.5&w=144&q=80"
+            alt=""
+            className="w-12 h-12 rounded-full"
+          />
+        </button>
+        <SearchControl onSearchSubmit={onSearchSubmit} />
+      </div>
+    </header>
   );
 }
