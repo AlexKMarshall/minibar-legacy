5f9f41797411c048a7154f5c6c7567dd5d664a9c
Alex Marshall <alex.k.marshall83@gmail.com>
2020-08-01 08:31:28 +0100

    Add space at the end of horizontal scroll list

--- client/src/containers/Discover.js
+++ client/src/containers/Discover.js
@@ -27,19 +27,21 @@ export default function Discover() {
       <h2 className="mb-4 text-2xl font-display">Featured drinks</h2>
       <ul className="flex px-6 -mx-6 overflow-x-auto">
         {featuredDrinks.map((drink) => (
-          <li key={drink._id} className="flex-shrink-0 w-3/4 mr-4">
-            <Link to={`/recipe/${drink._id}`}>
-              <div className="relative overflow-hidden rounded-lg pb-5/6">
-                <img
-                  src={drink.image}
-                  alt={drink.name}
-                  className="absolute object-cover w-full h-full"
-                />
-              </div>
-              <h3 className="py-4 text-lg font-semibold font-display">
-                {drink.name}
-              </h3>
-            </Link>
+          <li key={drink._id} className="flex-shrink-0 w-3/4">
+            <div className="mr-4">
+              <Link to={`/recipe/${drink._id}`}>
+                <div className="relative overflow-hidden rounded-lg pb-5/6">
+                  <img
+                    src={drink.image}
+                    alt={drink.name}
+                    className="absolute object-cover w-full h-full"
+                  />
+                </div>
+                <h3 className="py-4 text-lg font-semibold font-display">
+                  {drink.name}
+                </h3>
+              </Link>
+            </div>
           </li>
         ))}
       </ul>
