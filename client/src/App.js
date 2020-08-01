ae2fd4d962889075c18ad36566ada90238cab8ba
Alex Marshall <alex.k.marshall83@gmail.com>
2020-08-01 08:29:11 +0100

    Update background color

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ import Layout from "./components/Layout";
 
 function App() {
   return (
-    <div className="max-w-screen-sm mx-auto">
+    <div className="max-w-screen-sm min-h-screen mx-auto bg-gray-300">
       <Router>
         <Switch>
           <Route path="/recipe/:id">
